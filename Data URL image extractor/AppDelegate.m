8f85f6c1a83672aa4854bf49f0556997a653c6db
Nicolas Mahe <Nico@Nicolass-MacBook-Pro.local>
2014-11-06 23:25:38 +0100

    Quit app when last window is closed

--- Data URL image extractor/AppDelegate.m
+++ Data URL image extractor/AppDelegate.m
@@ -22,4 +22,10 @@
     // Insert code here to tear down your application
 }
 
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
+    return YES;
+}
+
+
 @end
